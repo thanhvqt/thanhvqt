4220f4c8211b078de818f02bdcf1789c8eb63885
thanhvqt <vuongquythanh07@gmail.com>
2024-08-19 15:05:16 +0700

    .

---
+++ tài liệu cũ/my-app/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render( < h1 > Hello, world! < /h1> );
+            return ( < div id = "root" > < /div>)
+            }
+            export default App;
---
+++ tài liệu cũ/uutyu/App.js
@@ -0,0 +1,7 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    <h1>Hello world</h1>
+            }
+            export default App;
